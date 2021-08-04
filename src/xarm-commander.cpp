2bb228ee218221c3ac4da149e69fcb412d54491c
jo-bru <jonas.bruetsch@hotmail.com>
2021-08-04 12:28:25 +0200

    refactor: magic numbers, range-based loop, TODO comments, pointer init, auto arm init (resolves: #9)

--- src/xarm-commander.cpp
+++ src/xarm-commander.cpp
@@ -1,14 +1,22 @@
 #include <CLI11.hpp>
 #include <xarm/wrapper/xarm_api.h>
 
-#include <string>
 #include <iostream>
+#include <string>
 
 int main(int argc, char **argv)
 {
+    // Default values (to avoid magic numbers) => not sure if this is the way to go (maybe using namespace, seperate headerfile?)
+    constexpr float kDefaultPosX = 300;
+    constexpr float kDefaultPosY = 0;
+    constexpr float kDefaultPosZ = 200;
+    constexpr float kDefaultPosRoll = 180; // [Deg] by default
+    constexpr float kDefaultPosPitch = 0;  // [Deg] by default
+    constexpr float kDefaultPosYaw = 0;    // [Deg] by default
 
-    XArmAPI *arm;
-    int res = 1;
+    constexpr int kAllServo = 8;
+
+    int res{1};
 
     CLI::App app{"xarm-commander: a command line tool for controlling the xArm7."};
 
@@ -17,7 +25,7 @@ int main(int argc, char **argv)
 
     // ===== FLAGS =====
     bool print_mode{false};
-    app.add_flag("-v, --verbose", print_mode, "verbose mode."); // TODO: multilevel verbose modes + logger
+    app.add_flag("-v, --verbose", print_mode, "verbose mode."); // TODO(jo-bru): multilevel verbose modes + logger
 
     // ===== OPTIONS =====
     std::string port; // 130.82.171.9
@@ -32,11 +40,11 @@ int main(int argc, char **argv)
     bool enable_flag{true};
     motion_enable->add_flag("-e, --enable, -d{false}, --disable{false}", enable_flag, "enable or disable the xArm (default: --enable)");
 
-    int servo_option{8};
+    int servo_option{kAllServo};
     motion_enable->add_option("-s, --servo", servo_option, "choose servo [1-8] to be enabled/disabled, (default: 8 - enable/disable all servo)");
 
     motion_enable->callback([&]() {
-        arm = new XArmAPI(port);
+        auto *arm = new XArmAPI(port);
         res = arm->motion_enable(enable_flag, servo_option);
 
         if (print_mode)
@@ -46,13 +54,13 @@ int main(int argc, char **argv)
     });
 
     // Subcommand: set_state
-    auto set_state = app.add_subcommand("set_state", "send a set_state command");
+    auto *set_state = app.add_subcommand("set_state", "send a set_state command");
 
     int state_option{0};
     set_state->add_option("-s, --state", state_option, "state, 0: sport, 3: pause, 4: stop");
 
     set_state->callback([&]() {
-        arm = new XArmAPI(port);
+        auto *arm = new XArmAPI(port);
         res = arm->set_state(state_option);
 
         if (print_mode)
@@ -62,13 +70,13 @@ int main(int argc, char **argv)
     });
 
     // Subcommand: set_mode
-    auto set_mode = app.add_subcommand("set_mode", "send a set_mode command");
+    auto *set_mode = app.add_subcommand("set_mode", "send a set_mode command");
 
     int mode_option = 0;
     set_mode->add_option("-m", mode_option, "mode, 0: position control mode, 1: servo motion mode, 2: joint teaching mode, 3: cartesian teaching mode (invalid), 4: simulation mode");
 
     set_mode->callback([&]() {
-        arm = new XArmAPI(port);
+        auto *arm = new XArmAPI(port);
         res = arm->set_mode(mode_option);
 
         if (print_mode)
@@ -78,10 +86,10 @@ int main(int argc, char **argv)
     });
 
     // Subcommand: get_version
-    auto get_version = app.add_subcommand("get_version", "send a get_version command");
+    auto *get_version = app.add_subcommand("get_version", "send a get_version command");
     get_version->callback([&]() {
-        arm = new XArmAPI(port);
-        res = arm->get_version(arm->version); // TODO: check if this is even needed or arm->version holds an updated version
+        auto *arm = new XArmAPI(port);
+        res = arm->get_version(arm->version); // TODO(jo-bru): check if this is even needed or arm->version holds an updated version
 
         if (print_mode)
         {
@@ -92,9 +100,9 @@ int main(int argc, char **argv)
     });
 
     // Subcommand: get_state
-    auto get_state = app.add_subcommand("get_state", "send a get_state commmand");
+    auto *get_state = app.add_subcommand("get_state", "send a get_state commmand");
     get_state->callback([&]() {
-        arm = new XArmAPI(port);
+        auto *arm = new XArmAPI(port);
         res = arm->get_state(&arm->state);
 
         if (print_mode)
@@ -106,9 +114,9 @@ int main(int argc, char **argv)
     });
 
     // Subcommand: get_position
-    auto get_position = app.add_subcommand("get_position", "send a get_position command to get the cartesian position");
+    auto *get_position = app.add_subcommand("get_position", "send a get_position command to get the cartesian position");
     get_position->callback([&]() {
-        arm = new XArmAPI(port);
+        auto *arm = new XArmAPI(port);
         res = arm->get_position(arm->position);
 
         if (print_mode)
@@ -125,26 +133,26 @@ int main(int argc, char **argv)
     });
 
     // Subcommand: set_position
-    auto set_position = app.add_subcommand("set_position", "send a set_position command to get the cartesian position");
+    auto *set_position = app.add_subcommand("set_position", "send a set_position command to get the cartesian position");
 
-    float x_option = 300;
+    float x_option = kDefaultPosX;
     set_position->add_option("-x", x_option, "x(mm)");
-    float y_option = 0;
+    float y_option = kDefaultPosYaw;
     set_position->add_option("-y", y_option, "y(mm)");
-    float z_option = 200;
+    float z_option = kDefaultPosZ;
     set_position->add_option("-z", z_option, "z(mm)");
-    float roll_option = 180;
+    float roll_option = kDefaultPosRoll;
     set_position->add_option("-r, --roll", roll_option, "roll(rad or °)");
-    float pitch_option = 0;
+    float pitch_option = kDefaultPosPitch;
     set_position->add_option("-p, --pitch", pitch_option, "pitch(rad or °)");
-    float yaw_option = 0;
+    float yaw_option = kDefaultPosYaw;
     set_position->add_option("-w, --yaw", yaw_option, "yaw(rad or °)");
 
     bool wait_option = false;
     set_position->add_option("--wait", wait_option, "whether to wait for the arm to complete");
 
     set_position->callback([&]() {
-        arm = new XArmAPI(port);
+        auto *arm = new XArmAPI(port);
 
         float pose[6];
         pose[0] = x_option;
@@ -161,8 +169,11 @@ int main(int argc, char **argv)
             std::cout << "Set position - Response: " << res << "\n"
                       << "Position: ";
             std::cout << "[ ";
-            for (int i = 0; i < 6; i++)
-                std::cout << pose[i] << " ";
+            // range-based for loop
+            for (float position : pose)
+            {
+                std::cout << position << " ";
+            }
             std::cout << "]"
                       << "\n";
         }
